17914678c29e9c51e3c26155a31ff8bdf5e4c64b
libochun1 <libochun1@crland.com.cn>
2024-04-29 12:24:24 +0800

    init

---
+++ app/src/main/java/com/example/myapplication/network/ServerApi.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.network
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface ServerApi {
+    companion object {
+        val instance by lazy { retrofit.create(ServerApi::class.java) }
+    }
+
+    // 登录接口
+    @POST("/loginV2")
+    fun login(@Body request: LoginRequest): Call<Token>
+
+
+    // 直播记录上报接口
+    @POST("/admin/live/addRecords")
+    fun messageUpload(@Body request: LiveRecordRequest, @Header("Authorization") token: String): Call<ResponseResult<String>>
+}
